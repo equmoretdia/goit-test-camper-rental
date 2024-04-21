bd0b458922c841ed051a433c86831ed0d9553e4e
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-04-21 23:49:23 +0200

    store update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { campersReducer } from './campersSlice';
+import { favoritesReducer } from './favoritesSlice';
+
+const favoritesPersistConfig = {
+  key: 'favorites',
+  storage,
+  whitelist: ['favorites'],
+};
 
 export const store = configureStore({
   reducer: {
     campers: campersReducer,
+    favorites: persistReducer(favoritesPersistConfig, favoritesReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);

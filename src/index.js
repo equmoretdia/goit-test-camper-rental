a2042a698b1644957f2a5f345971584a421b4f08
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-04-21 23:51:14 +0200

    index.js and selectors update

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from 'components/App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import './styles/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-test-camper-rental">
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="/goit-test-camper-rental">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,4 @@
 export const selectItems = state => state.campers.items;
 export const selectIsLoading = state => state.campers.isLoading;
 export const selectError = state => state.contacts.error;
+export const selectFavorites = state => state.favorites.items;

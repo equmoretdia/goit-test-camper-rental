5e389446f1f58d88e5bee01aae87eb7afa1d8e28
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-04-25 23:37:29 +0200

    Home page content is added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,76 @@
 import React from 'react';
 
-import { Header } from './PageStyles';
+// import { Header } from './PageStyles';
 
 const Home = () => {
   return (
     <section>
-      <Header>Home</Header>
+      <h1>Seven Reasons to Choose Us!</h1>
+      <ol>
+        <li>
+          <h2>Wide Range of Campers: </h2>
+          <p>
+            We offer a diverse selection of campers to suit every traveler's
+            needs, from cozy vans perfect for couples to spacious
+            fully-integrated models ideal for families and groups of friends.
+          </p>
+        </li>
+        <li>
+          <h2>Top-Quality Equipment: </h2>
+          <p>
+            Our campers are equipped with modern amenities to ensure a
+            comfortable and convenient travel experience. From fully-equipped
+            kitchens to luxurious bathrooms, we provide everything you need for
+            a memorable journey.
+          </p>
+        </li>
+        <li>
+          <h2>Reliable and Well-Maintained: </h2>
+          <p>
+            We take pride in maintaining our campers to the highest standards of
+            safety and cleanliness. Each vehicle undergoes regular inspections
+            and maintenance checks to ensure reliability on the road.
+          </p>
+        </li>
+        <li>
+          <h2>Flexible Rental Options: </h2>
+          <p>
+            Whether you're planning a weekend getaway or a month-long adventure,
+            we offer flexible rental options to accommodate your travel plans.
+            Choose from daily, weekly, or monthly rental periods to suit your
+            schedule.
+          </p>
+        </li>
+        <li>
+          <h2>Exceptional Customer Service: </h2>
+          <p>
+            Our team is dedicated to providing exceptional customer service from
+            start to finish. We're here to assist you every step of the way,
+            from selecting the perfect camper to providing support during your
+            trip.
+          </p>
+        </li>
+        <li>
+          <h2>Affordable Rates: </h2>
+          <p>
+            We believe that unforgettable travel experiences should be
+            accessible to everyone. That's why we offer competitive rates and
+            transparent pricing with no hidden fees, so you can enjoy your
+            journey without breaking the bank.
+          </p>
+        </li>
+        <li>
+          <h2>Explore Ukraine: </h2>
+          <p>
+            With our campers, you have the freedom to explore Ukraine at your
+            own pace. Whether you're drawn to the stunning landscapes of the
+            Carpathian Mountains, the vibrant culture of Lviv, or the historic
+            charm of Kyiv, our campers provide the ultimate flexibility to
+            discover the beauty of Ukraine.
+          </p>
+        </li>
+      </ol>
+      <h2>Celebrate Your Dreams Today, Seize the Adventure Now!</h2>
     </section>
   );
 };

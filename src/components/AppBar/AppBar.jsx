1968ab0e2f73ddb1d49660388f27955f3c050847
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-05-02 21:47:38 +0200

    AppBar is styled

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { Navigation } from 'components/Navigation/Navigation';
+import { Nav, LogoContainer, LogoName, LogoAccent } from './AppBarStyles';
+import camperLogo from 'images/camper.png';
 
 export const AppBar = () => {
   return (
     <header>
-      <nav>
+      <Nav>
         <div>
-          <NavLink to="/">Logo</NavLink>
+          <NavLink to="/">
+            <LogoContainer>
+              <img
+                src={camperLogo}
+                alt="camper against the backdrop of forested mountains and a river"
+                width={70}
+                height={70}
+              />
+              <LogoName>
+                <LogoAccent>Ukrainian</LogoAccent>Camper
+              </LogoName>
+            </LogoContainer>
+          </NavLink>
         </div>
         <Navigation />
-      </nav>
+      </Nav>
     </header>
   );
 };
---
+++ src/components/AppBar/AppBarStyles.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 32px;
+  margin-bottom: 32px;
+  border-bottom: 1px solid var(--card-border-color);
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const LogoName = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  color: var(--button);
+`;
+
+export const LogoAccent = styled.span`
+  color: var(--rating);
+`;

a9c19888b0ee78c3ab59f34eda8117b39804f8e3
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-05-03 22:55:29 +0200

    Modal fix: all content is resided in modal window

--- src/components/Modal/ModalStyles.jsx
+++ src/components/Modal/ModalStyles.jsx
@@ -6,17 +6,19 @@ const Backdrop = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
+  overflow-y: auto;
   background-color: var(--modal-backdrop);
 `;
 
 const Content = styled.div`
-  position: absolute;
-  top: 50%;
+  position: relative;
+  /* top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   padding: 40px;
   width: 982px;
-  height: 720px;
+  margin: 40px auto;
+  /* height: 720px; */
   border-radius: 20px;
   background-color: var(--background);
 `;
